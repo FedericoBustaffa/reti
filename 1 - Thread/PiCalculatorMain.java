84e397d360f0f617264d208f510498cd8d253e53
FedericoBustaffa <kiko.bustaffa@gmail.com>
2022-05-30 15:15:42 +0200

    PiCalculator

--- 1 - Thread/PiCalculatorMain.java
+++ 1 - Thread/PiCalculatorMain.java
@@ -1,20 +1,20 @@
 
-public class MainClass {
+public class PiCalculatorMain {
 
 	public static void main(String[] args) {
 
 		// prendo l'input da riga di comando e lo converto in double e int
 		double accuracy = Double.parseDouble(args[0]); // precisione
 		int waiting_time = Integer.parseInt(args[1]); // tempo di timeout in millisecondi
-		
+
 		PiCalculator calc = new PiCalculator(accuracy);
 		Thread t = new Thread(calc);
 		t.start();
-		
+
 		try {
 			// l'esecuzione del main procede se scade il tempo contenuto in "waiting_time"
 			t.join(waiting_time);
-		} catch(InterruptedException e) {
+		} catch (InterruptedException e) {
 			System.out.println(e.toString());
 		}
 		// interruzione del thread calcolatore
