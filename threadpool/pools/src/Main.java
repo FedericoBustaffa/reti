c9fef753db47d0578e152d3326142991dcb1f4b8
FedericoBustaffa <kiko.bustaffa@gmail.com>
2022-06-04 18:08:14 +0200

    pools

--- threadpool/pools/src/Main.java
+++ threadpool/pools/src/Main.java
@@ -1,22 +1,29 @@
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newCachedThreadPool();
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(
+                0,
+                10,
+                0L,
+                TimeUnit.SECONDS,
+                new SynchronousQueue<Runnable>());
+
         BlockingQueue<Integer> queue = new LinkedBlockingQueue<Integer>();
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 30; i++) {
             executor.execute(new Consumer(queue));
             executor.execute(new Producer(queue, i));
         }
         executor.shutdown();
 
         // Potrebbe risvegliarsi con una chiamata spuria
-        executor.awaitTermination(5, TimeUnit.SECONDS);
+        while (!executor.isTerminated())
+            ;
 
         System.out.println("ThreadPool size: " + executor.getPoolSize());
         System.out.println("Queue size: " + queue.size());
