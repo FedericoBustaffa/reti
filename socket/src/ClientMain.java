44a193a9b854d34186d0d8e74b51283882f464d8
FedericoBustaffa <kiko.bustaffa@gmail.com>
2022-06-08 02:02:20 +0200

    socket

--- socket/src/ClientMain.java
+++ socket/src/ClientMain.java
@@ -3,16 +3,15 @@ import java.net.InetAddress;
 import java.net.Socket;
 
 public class ClientMain {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
-        Socket client = null;
-        try {
-            client = new Socket(InetAddress.getLocalHost(), 1);
-            System.out.println("Service on port " + client.getPort());
-        } catch (IOException e) {
-            System.out.println("Port " + client + " not available");
-        } finally {
-            client.close();
+        for (int i = 1024; i < 2048; i++) {
+            try (Socket client = new Socket(InetAddress.getLocalHost(), i)) {
+                System.out.println("Server at port: " + i);
+                break;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
     }
--- socket/src/ServerMain.java
+++ socket/src/ServerMain.java
@@ -1,28 +1,32 @@
 import java.io.IOException;
+import java.net.BindException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ServerMain {
 	public static void main(String[] args) throws IOException {
 		ServerSocket server_socket = null;
-		for (int i = 1; i < 1024; i++) {
+		for (int i = 1; i < 2048; i++) {
 			try {
 				server_socket = new ServerSocket(i);
 				if (server_socket.isBound()) {
 					System.out.println("Listening on port " + server_socket.getLocalPort());
 					break;
 				}
-			} catch (IOException e) {
-				System.out.println("Port " + i + "not available");
+			} catch (BindException e) {
+				System.out.println("Port " + i + " not available");
 			}
 		}
 
-		try (Socket connection = server_socket.accept()) {
-			System.out.println("client connesso");
-		} catch (IOException e) {
-			e.printStackTrace();
+		if (server_socket != null) {
+			try (Socket connection = server_socket.accept()) {
+				System.out.println("Client connected");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 
 		server_socket.close();
+
 	}
 }
