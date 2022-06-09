59806f7da38e678c8befa9b80bb585d5b247956f
FedericoBustaffa <kiko.bustaffa@gmail.com>
2022-06-09 16:44:14 +0200

    client

--- socket/src/Client.java
+++ socket/src/Client.java
@@ -9,66 +9,68 @@ import java.net.SocketException;
 import java.util.Scanner;
 
 public class Client {
-    private String name;
-    private Socket socket;
-    private BufferedWriter writer;
+	private String name;
+	private Socket socket;
+	private BufferedWriter writer;
 
-    public Client(String name) throws NullPointerException {
-        this.name = name;
-        socket = new Socket();
-    }
+	public Client(String name) throws NullPointerException {
+		this.name = name;
+		socket = new Socket();
+	}
 
-    public void connect() {
-        try {
-            socket.connect(new InetSocketAddress(InetAddress.getByName("127.0.1.1"), 1500));
-            writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+	public void connect() {
+		try {
+			socket.connect(new InetSocketAddress(InetAddress.getByName("192.162.1.21"), 1500));
+			writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
-            writer.write(name + "\n");
-            writer.flush();
-        } catch (ConnectException e) {
-        } catch (SocketException e) {
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+			writer.write(name + "\n");
+			writer.flush();
+		} catch (ConnectException e) {
+			System.out.println("Connection error");
+		} catch (SocketException e) {
+			System.out.println("Socket error");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void write(String msg) {
-        try {
-            writer.write(msg + "\n");
-            writer.flush();
-        } catch (IOException e) {
-            System.out.println("Writing error");
-        }
-    }
+	public void write(String msg) {
+		try {
+			writer.write(msg + "\n");
+			writer.flush();
+		} catch (IOException e) {
+			System.out.println("Writing error");
+		}
+	}
 
-    public void close() {
-        try {
-            writer.close();
-            socket.close();
-        } catch (IOException e) {
-            System.out.println("Error on close");
-        }
-    }
+	public void close() {
+		try {
+			writer.close();
+			socket.close();
+		} catch (IOException e) {
+			System.out.println("Error on close");
+		}
+	}
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Client name: ");
-        Client client = new Client(in.nextLine());
-        client.connect();
-        System.out.println("Service on port 1500");
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Client name: ");
+		Client client = new Client(in.nextLine());
+		client.connect();
+		System.out.println("Service on port 1500");
 
-        String msg;
-        do {
-            System.out.print("Write to server: ");
-            msg = in.nextLine();
-            client.write(msg);
-        } while (!msg.equals("close"));
+		String msg;
+		do {
+			System.out.print("Write to server: ");
+			msg = in.nextLine();
+			client.write(msg);
+		} while (!msg.equals("close"));
 
-        in.close();
-        client.close();
-    }
+		in.close();
+		client.close();
+	}
 }
