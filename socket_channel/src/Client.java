02290ddd4cf0d52896f96374df050975c4017f0a
FedericoBustaffa <kiko.bustaffa@gmail.com>
2022-06-11 11:44:33 +0200

    socket_channel

---
+++ socket_channel/src/Client.java
@@ -0,0 +1,9 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) throws Exception {
+        ServerSocket socket = new ServerSocket();
+
+    }
+}
